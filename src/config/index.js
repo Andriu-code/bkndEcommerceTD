5abd302aa4f1d5aeec52667ee37c868215101f97
Andriu <a.lealhernandez91@gmail.com>
2025-09-06 21:04:20 -0600

    backendTD

--- src/config/index.js
+++ src/config/index.js
@@ -5,3 +5,9 @@ module.exports.Config = {
     mongoUri: process.env.MONGO_URI,
     mongoDbname: process.env.MONGO_DBNAME,
 }
+
+console.log('variables')
+console.log(process.env.PORT)
+console.log(process.env.MONGO_URI)
+console.log(process.env.MONGO_DBNAME)
+
